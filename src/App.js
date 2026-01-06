8bd5620ddc11ab2451494b4700ee0598a9b2f856
David MARTINS <david.joaquim.martins@gmail.com>
2026-01-06 02:43:19 +0100

    localise me

--- src/App.js
+++ src/App.js
@@ -9,33 +9,58 @@ function App() {
   () => localStorage.getItem('city') || null
 );
 
-    useEffect(() => {
-    if (!navigator.geolocation) return;
+  //   useEffect(() => {
+  //   if (!navigator.geolocation) return;
 
-    navigator.geolocation.getCurrentPosition(
-      (position) => {
-        const { latitude, longitude } = position.coords;
-        console.log("latitude :",latitude);
-        console.log("longitude :",longitude);
+  //   navigator.geolocation.getCurrentPosition(
+  //     (position) => {
+  //       const { latitude, longitude } = position.coords;
+  //       console.log("latitude :",latitude);
+  //       console.log("longitude :",longitude);
 
-        fetch(
-          `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}&localityLanguage=fr`
-        )
-          .then(res => res.json())
-          .then(data => {
-            const detectedCity = data.city || data.locality;
-            console.log("detectedCity :",detectedCity);
-            if (detectedCity) {
-              setCity(detectedCity);
-              localStorage.setItem('city', detectedCity);
-            }
+  //       fetch(
+  //         `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}&localityLanguage=fr`
+  //       )
+  //         .then(res => res.json())
+  //         .then(data => {
+  //           const detectedCity = data.city || data.locality;
+  //           console.log("detectedCity :",detectedCity);
+  //           if (detectedCity) {
+  //             setCity(detectedCity);
+  //             localStorage.setItem('city', detectedCity);
+  //           }
               
-          })
-          .catch(() => setCity('Paris'));
-      },
-      () => setCity('Paris')
-    );
-  }, []);
+  //         })
+  //         .catch(() => setCity('Paris'));
+  //     },
+  //     () => setCity('Paris')
+  //   );
+  // }, []);
+
+  useEffect(() => {
+  if (city !== null) return; // 🛑 on a déjà une ville
+
+  if (!navigator.geolocation) return;
+
+  navigator.geolocation.getCurrentPosition(
+    (position) => {
+      const { latitude, longitude } = position.coords;
+
+      fetch(
+        `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}&localityLanguage=fr`
+      )
+        .then(res => res.json())
+        .then(data => {
+          const detectedCity = data.city || data.locality;
+          if (detectedCity) {
+            setCity(detectedCity);
+            localStorage.setItem('city', detectedCity);
+          }
+        });
+    }
+  );
+}, [city]);
+
 
   useEffect(() => {
     fetch(`https://api.weatherapi.com/v1/forecast.json?key=f9cc340e26b240188b2195245242805&q=${city}&days=7&aqi=no&alerts=no`)
